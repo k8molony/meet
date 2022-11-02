bd6a3fb76d110e0b877434b53e41aa5112a1a78c
Katy Molony <85511799+k8molony@users.noreply.github.com>
2022-11-02 11:50:35 -0400

    4.3 complete number of events testing

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -45,50 +45,8 @@ class NumberOfEvents extends Component {
 
 export default NumberOfEvents;
 
-// import React, { Component } from 'react';
+// import React, { Component } from 'react';                                                                                                                                                        1fmrt5iukm
 
-// class NumberOfEvents extends Component {
-//   state = {
-//     NumberOfEvents: 32,
-//     errorText: '',
-//   };
-
-//   handleInputChanged = (event) => {
-//     const value = event.target.value;
-//     if (value < 1 || value > 32) {
-//       this.setState({
-//         numberOfEvents: value,
-//         errorText: 'Enter number from 1-32',
-//       });
-//     } else {
-//       this.setState({
-//         numberOfEvents: event.target.value,
-//         errorText: '',
-//       });
-//     }
-
-//     this.props.updateEvents(undefined, value);
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <div className='number-of-events'>
-//           <label>
-//             Number of Events: 
-//             <input
-//               type="number"
-//               className="number-of-events-input"
-//               min="1"
-//               value={this.state.numberOfEvents}
-//               onChange={this.handleInputChanged}
-//             />
-//           </label>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
 
 // class NumberOfEvents extends Component {
 //   state= {
--- src/_tests_/NumberOfEvents.test.js
+++ src/_tests_/NumberOfEvents.test.js
@@ -3,7 +3,9 @@
 /* 
 Tests:
 - DONE render number of events element
-- DONE render input box for number
+- DONE render input box for number of events
+- DONE render number of events correctly
+- number of events rendered matches input when changed
 - DONE default number of events to 32
 */
 
@@ -18,12 +20,17 @@ describe('<NumberOfEvents /> component', () => {
     NumberOfEventsWrapper = shallow(<NumberOfEvents />);
   });
 
-  test('render element', () => {
+  test('render number of events element', () => {
     expect(NumberOfEventsWrapper.find('.number-of-events')).toHaveLength(1);
   });
 
-  test('render input for number of events', () => {
+  test('render input box for number of events', () => {
     expect(NumberOfEventsWrapper.find('.number-of-events-input')).toHaveLength(1);
+  });
+
+  test('render number of events correctly', () => {
+    const numberQuery = NumberOfEventsWrapper.state('numberQuery');
+    expect(NumberOfEventsWrapper.find('.number-of-events').prop('value')).toBe(numberQuery);
   });
 
   test('render default input for number of events of 32', () => {
