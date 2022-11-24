de9d34f72bed4f01c6921fa579898f411cf2d46e
Katy Molony <85511799+k8molony@users.noreply.github.com>
2022-11-23 22:15:19 -0500

    4.5 installed older version of puppeteer

--- src/_tests_/EndToEnd.test.js
+++ src/_tests_/EndToEnd.test.js
@@ -4,6 +4,7 @@ describe('show/hide an event details', () => {
   let browser;
   let page;
   beforeAll(async () => {
+
     jest.setTimeout(30000);
     browser = await puppeteer.launch({
       headless: false,
