040ef3cbafac71398b492aa733a0bc894480655b
Katy Molony <85511799+k8molony@users.noreply.github.com>
2022-12-21 17:39:11 -0500

    4.8 added token verification to componentDid Mount

--- src/App.js
+++ src/App.js
@@ -40,14 +40,18 @@ class App extends Component {
 
   async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ 
-          events, 
-          locations: extractLocations(events)
-        });
-      }
-    });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
   }
 
 
@@ -58,7 +62,7 @@ class App extends Component {
 
   render() {
     const { locations, events, numberOfEvents } = this.state;
-    
+
     if (this.state.showWelcomeScreen === undefined) return <div className="App" />
     return (
       <div className="App">
