3d571690a1efdf714b820d7cff9eacd305f76f9a
Katy Molony <85511799+k8molony@users.noreply.github.com>
2023-01-09 12:32:34 -0500

    4.9 added pie chart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import WelcomeScreen from './WelcomeScreen';
+import EventGenre from './EventGenre';
 
 import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import { OfflineAlert } from './Alert';
@@ -99,17 +100,20 @@ class App extends Component {
         />
         <h4>Events in each city</h4>
 
-        <ResponsiveContainer height={400} >
-          <ScatterChart margin={{
-              top: 20, right: 20, bottom: 20, left: 20,
-            }}>
-            <CartesianGrid />
-            <XAxis type="category" dataKey="city" name="City" />
-            <YAxis allowDecimals={false} type="number" dataKey="number" name="Number of Events" />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          </ScatterChart>
-        </ResponsiveContainer>
+        <div className='data-vis-wrapper'>
+          <EventGenre events={events} />
+          <ResponsiveContainer height={400} >
+            <ScatterChart margin={{
+                top: 20, right: 20, bottom: 20, left: 20,
+              }}>
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="City" />
+              <YAxis allowDecimals={false} type="number" dataKey="number" name="Number of Events" />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
         <EventList  events={events} />
         <WelcomeScreen 
           showWelcomeScreen={this.state.showWelcomeScreen} 
---
+++ src/EventGenre.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { PieChart, Pie, Cell, Legend, Tooltip, ResponsiveContainer } from "recharts";
+
+const EventGenre = ({ events }) => {
+  const [data, setData] = useState([]);
+  useEffect(() => { setData(() => getData()); }, [events]);
+
+  const colors = ["#c97484", "#808080", "#7daafb", "#65cca9", "#f79e69"];
+
+  const getData = () => {
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    const summaries = events.map(event => event.summary);
+    const rawResults = genres.map((genre) => {
+      const value = summaries.filter((summary) => summary.includes(genre)).length;
+      return {
+        name: genre,
+        value
+      }
+    });
+    const results = rawResults.filter((element) => element.value!==0);
+    return results;
+  }
+
+  return (
+    <ResponsiveContainer height={400} >
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx={200}
+          labelLine={true}
+          outerRadius={80}
+          dataKey="value"
+          label={({ name, percent }) => 
+            `${name} ${(percent * 100).toFixed(0)}%`
+          }
+        >
+          {data.map((entry, index) => (
+            <Cell key = { `cell-${index}`} fill={colors[index]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend verticalAlign="bottom" height={76}/>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+}
+
+export default EventGenre;
